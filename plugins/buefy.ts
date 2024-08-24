dbb0e66f7bbd246f0ec0f9838ec1daefcfccfd40
realSamy <imhoodaji@gmail.com>
2024-08-24 15:31:25 +0330

    Ignore auto import not recognized by IDE

--- plugins/buefy.ts
+++ plugins/buefy.ts
@@ -2,6 +2,7 @@ import 'buefy/dist/buefy.css';
 import Buefy from "buefy";
 import {type NuxtApp} from "nuxt/app";
 
+// @ts-ignore
 export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
     // @ts-ignore
     nuxtApp.vueApp.use(Buefy);
