0249205e9abb133c2c7c05ecd876007b98fbda63
realSamy <imhoodaji@gmail.com>
2024-08-24 15:09:47 +0330

    Import icon pack

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   modules: ['@pinia/nuxt'],
+  app: {
+    head: {
+      link: [
+        {'rel': 'stylesheet', 'href': 'https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css'}
+      ]
+    }
+  },
   devtools: { enabled: true }
 })
