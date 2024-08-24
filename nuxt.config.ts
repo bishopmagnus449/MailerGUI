a474bce85a91e67db292abff2d692fa5f9584948
realSamy <imhoodaji@gmail.com>
2024-08-24 15:11:22 +0330

    Add pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  modules: ['@pinia/nuxt'],
   app: {
     head: {
       link: [
@@ -9,5 +8,8 @@ export default defineNuxtConfig({
       ]
     }
   },
+  modules: [
+    '@pinia/nuxt',
+  ],
   devtools: { enabled: true }
 })
