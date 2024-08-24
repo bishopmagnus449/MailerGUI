6a55b843bccd4d015a46479c1a43dbe5011073b3
realSamy <imhoodaji@gmail.com>
2024-08-24 15:09:14 +0330

    type fix

--- plugins/buefy.ts
+++ plugins/buefy.ts
@@ -1,7 +1,8 @@
 import 'buefy/dist/buefy.css';
 import Buefy from "buefy";
+import {type NuxtApp} from "nuxt/app";
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
     // @ts-ignore
     nuxtApp.vueApp.use(Buefy);
 });
