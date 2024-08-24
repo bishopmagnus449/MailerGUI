1ef9f34fdc624076f41bdb03ceca51c6164debda
realSamy <imhoodaji@gmail.com>
2024-08-24 15:14:07 +0330

    Add auto-animate module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@pinia/nuxt',
     'nuxt-tiptap-editor',
+    '@formkit/auto-animate/nuxt'
   ],
   tiptap: {
     prefix: 'Tiptap',
