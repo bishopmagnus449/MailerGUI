b25e7ff6328585abe6b7eb7bc5438f7513cf062d
Saman Hoodaji <59841686+realSamy@users.noreply.github.com>
2024-10-22 01:12:42 +0330

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if ! command -v docker >/dev/null 2>&1; then
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl git
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+if [ -d "MailerGUI" ]; then
+  cd MailerGUI || exit 1
+  git pull
+else
+  git clone https://github.com/realSamy/MailerGUI && cd MailerGUI || exit 1
+fi
+
+clear
+
+sudo docker compose up
